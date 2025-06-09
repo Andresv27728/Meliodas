2c9843bd59dadbbfdeb6585f20c8ed6d864d9a90
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-06-09 14:28:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -41,15 +41,15 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 //let require = createRequire(megu)
 let { say } = cfonts
 
-console.log(chalk.bold.redBright(`\n✰ Iniciando kirit-Bot-MD ✰\n`))
+console.log(chalk.bold.redBright(`\n✰ Iniciando Meliodas-Bot-MD ✰\n`))
 
-say('kirit-Bot-MD', {
+say('meliodas-Bot-MD', {
 font: 'block',
 align: 'center',
 colors: ['magentaBright']
 })
 
-say(`Developed By • Deylin`, {
+say(`Developed By • Bajo`, {
 font: 'console',
 align: 'center',
 colors: ['blueBright']
@@ -212,7 +212,7 @@ if (opcion == '1' || methodCodeQR) {
 console.log(chalk.bold.yellow(`\n❐ ESCANEA EL CÓDIGO QR EXPIRA EN 45 SEGUNDOS`))}
 }
 if (connection == 'open') {
-console.log(chalk.bold.green('\n👑 kirit-Bot-MD Conectada con éxito 👑'))
+console.log(chalk.bold.green('\n👑 MELIODAS-Bot-MD Conectada con éxito 👑'))
 }
 let reason = new Boom(lastDisconnect?.error)?.output?.statusCode
 if (connection === 'close') {
